0398388d1e545df1b029dcc40e8b4bb52c205932
Shantanu Singh <sshantanuthakur1099@gmail.com>
2021-01-19 02:36:46 +0530

    Added style

--- script.js
+++ script.js
@@ -2,21 +2,37 @@ var btn = document.getElementsByClassName("btn1")[0];
 var input = document.querySelector("input");
 var ul = document.querySelector("ul");
 
+var checkAndAddTask = (event) => {
+    if(input.value.length > 0 && event.keyCode === 13){
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value = "";  
+    }
+};
+var listNotEmpty = () => {
+    if(input.value.length > 0){
+        return true;
+    }
+};
+
+var addTask = () => {
+    var li = document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value = "";
+};
+
 btn.addEventListener('click', function(event) {
-   if(input.value.length > 0){
-       var li = document.createElement("li");
-       li.appendChild(document.createTextNode(input.value));
-       ul.appendChild(li);
-       input.value = "";
+   if(listNotEmpty){
+       addTask();
    } 
 });
 // console.log("Live Still Working");
 
-input.addEventListener('keypress', (event) => {
-    if(input.value.length > 0 && event.which === 13){
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value = "";
-    }
-});
+// input.addEventListener('keypress', (event) => {
+//     if(listNotEmpty() && event.which === 13){
+//         addTask();
+//     }
+// });
+input.addEventListener('keypress', checkAndAddTask)
